60041fe04cc74debf6162ac2fede81a9c6431621
tusharmore26 <125184538+tusharmore26@users.noreply.github.com>
2023-03-19 23:42:41 +0530

    Add files via upload

---
+++ strings.js
@@ -0,0 +1,23 @@
+str="Welcome"
+console.log(str)
+
+// accessing string using index value
+console.log(str[0])
+console.log(str[5])
+
+// length of a string
+console.log(str.length)
+
+// escape character
+str2='Tushar\'s Car' //using escape we can add single quote under the string enclosed by single quotes.
+console.log(str2)
+str3="Welcome \"Tushar\""
+console.log(str3)
+str4="he\\she"
+console.log(str4)
+
+// template literals
+name1="Raj";
+name2="Henrry";
+str5=`${name1} is a friend of ${name2} `
+console.log(str5)
