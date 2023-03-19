41854585126d5d0637e798cc82350a1ecf4f4058
tusharmore26 <125184538+tusharmore26@users.noreply.github.com>
2023-03-20 00:11:47 +0530

    Add files via upload

---
+++ strings_methods.js
@@ -0,0 +1,32 @@
+// length of string
+str="Welcome Tushar"
+console.log(str.length)
+
+// touppercase() method
+console.log(str.toUpperCase())
+
+// tolowercase() mathod
+console.log(str.toLowerCase())
+
+// slice() Mehtod
+console.log(str.slice(3,14))
+
+// replace() Method
+console.log(str.replace("Welcome","Hii"))
+
+// substring() Method
+console.log(str.substring(8,14))
+
+// sunstr() method
+console.log(str.substr(8,14))
+
+// replaceall() method
+str2="his name is tushar and tushar is in 3rd year"
+console.log(str2.replaceAll("tushar","Raj"))
+
+// trim() method
+str3="   Pune    "
+console.log(str3)
+console.log(str3.trim())
+console.log(str3.trimStart(str3))
+console.log(str3.trimEnd(str3))
